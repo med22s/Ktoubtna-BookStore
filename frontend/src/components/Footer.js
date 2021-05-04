849d7f4da4f6757c7b99acb6f11d4cc7fb91c868
ryackerman <khalidtaleb51001@gmail.com>
2021-05-04 16:19:53 +0000

    adding social media icons with MDB

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Col,Row,Container} from 'react-bootstrap'
+import { MDBIcon, MDBContainer } from 'mdbreact'
 
 const Footer = () => {
     return (
@@ -7,6 +8,19 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className='text-center py-3'>
+                        <Col className="socialm">
+                            <MDBContainer>
+                                <a href="#!" className="tw-ic mr-3">
+                                    <MDBIcon fab icon="twitter" />
+                                </a>
+                                <a href="#!" className="fb-ic mr-3">
+                                    <MDBIcon fab icon="facebook-f" />
+                                </a>
+                                <a href="#!" className="git-ic mr-3">
+                                    <MDBIcon fab icon="instagram" />
+                                </a>
+                            </MDBContainer>
+                        </Col>
                         Copyright &copy; Ktoubtna
                     </Col>
                 </Row>
