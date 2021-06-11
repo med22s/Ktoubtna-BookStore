7cf363e246d67569c5d213183e6c63bfa9d7482f
khalidlaaroussi20 <khalidlaaroussi20@gmail.com>
2021-06-11 19:18:17 +0100

    fix attribute reviws in bookSchema

--- backend/Models/book.js
+++ backend/Models/book.js
@@ -11,14 +11,21 @@ const {serverHost,serverPort} = require('../config/config');
 * utils
 */
 const { APIError }            = require('../utils/errorHandler');
+
 /*
-const reviewSchema = mongoose.Schema({
-
-},
-{
-
-})
+* schema
 */
+
+const reviewSchema = mongoose.Schema({
+        userId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true, index: true, unique: true},
+        rating:{type:Number,required:true},
+        message:{type:String,required:true}
+    },
+    {
+        timestamps:true
+    }
+);
+
 const bookSchema   = mongoose.Schema({
     user:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
     name:{type:String,required:true},
@@ -26,12 +33,7 @@ const bookSchema   = mongoose.Schema({
     image:{type:String,required:true},
     description:{type:String,required:true},
     genre:{type:String,required:true},
-    reviews:[new mongoose.Schema({
-        name:{type:String,required:true},
-        rating:{type:Number,required:true},
-        message:{type:String,required:true}
-    },
-    {timestamps:true})],
+    reviews:[reviewSchema],
     rating:{type:Number,required:true,default:0},
     price:{type:Number,required:true,default:0},
     numberInStock:{type:Number,required:true,default:0},
